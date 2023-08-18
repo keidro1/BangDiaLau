a3ba58a4d9da0765c0c3ae09d525289eea55f275
My Nguyen <my.nguyen@navercorp.com>
2023-08-18 10:29:36 +0700

    Fix warning

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import MusicPage from '../pages/MusicPage';
