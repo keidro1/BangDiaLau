fd7f18a0ea2d6f6901612a7b6c2292e55dfcc3cb
Nguyen Viet My <zzvietmyzz@gmail.com>
2023-08-18 10:19:42 +0700

    #2 Migrate to TS (#3)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import MusicPage from './pages/MusicPage';
-import FavoritePage from './pages/FavoritePage';
-import ProfilePage from './pages/ProfilePage';
-import Navigation from './components/Navigation';
+import HomePage from '../pages/HomePage';
+import MusicPage from '../pages/MusicPage';
+import FavoritePage from '../pages/FavoritePage';
+import ProfilePage from '../pages/ProfilePage';
+import Navigation from '../components/Navigation';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Navigation />
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/music/:id" element={<MusicPage />} />
         <Route path="/favorites" element={<FavoritePage />} />
         <Route path="/profile" element={<ProfilePage />} />
