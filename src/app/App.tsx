15fd2f880a376cca5dc05b0e62ea83fef04b3890
My Nguyen <my.nguyen@navercorp.com>
2023-09-12 19:46:36 +0700

    Remove unused

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import MusicPage from '../pages/MusicPage';
-import FavoritePage from '../pages/FavoritePage';
-import ProfilePage from '../pages/ProfilePage';
 import LoadingLoginPage from '../pages/LoadingLoginPage';
 import { useAppDispatch, useAppSelector } from './store';
 import { fetchAuthInfo, fetchUserInfo } from './authReducers';
@@ -36,9 +33,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/music/:id" element={<MusicPage />} />
-        <Route path="/favorites" element={<FavoritePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/callback" element={<LoadingLoginPage />} />
       </Routes>
     </Router>
